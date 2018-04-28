abe58da756ebdb81c9e945aba834afc2b2a790b8
Josh Bailey <joshua.bailey@oit.edu>
2018-04-28 12:11:52 -0700

    CST238SRS02
    includes mockup in Android Studio, Steve Edgren's drawings, and my flowchart

---
+++ app/src/main/java/com/example/residentialremodelingestimator/SpecificsExampleActivity.java
@@ -0,0 +1,13 @@
+package com.example.residentialremodelingestimator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpecificsExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_specifics_example);
+    }
+}
